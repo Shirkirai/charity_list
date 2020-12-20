e1212956cc6269f81b8de963c4d8d28c3ff1df1f
Jung Lee <lee.jung.s95@gmail.com>
2020-12-20 06:52:17 +0000

    fix the syntax error 'end' issuegit add .

--- lib/charity_list/cli.rb
+++ lib/charity_list/cli.rb
@@ -15,7 +15,6 @@ class CharityList::CLI
       #can test by using the fake data below
       #Fake Data 1: CharityList::Charityorgs.new("AIDS")
       #Fake Data 2: CharityList::Charityorgs.new("VA")
-
       @category_list = CharityList::Category.all
   end
 
@@ -26,6 +25,7 @@ class CharityList::CLI
       end
   end
 
+
   def get_user_charity_category
       chosen_category = gets.strip.to_i
       show_charity_organizations_for(chosen_category) if valid_input(chosen_category, @category_list)
@@ -34,6 +34,7 @@ class CharityList::CLI
       end
   end
 
+
   def valid_input(input, data)
       input.to_i <= data.length && input.to_i > 0
   end
@@ -41,29 +42,22 @@ class CharityList::CLI
   def show_charity_organizations_for(chosen_category)
       #category is the category within the @@all array corresponding to the user input.
       #for example, if the user input is 2, then it'll take the 2nd entry from the array. In this case, it'll be 2. AIDS Charities
-      category = @category_list[chosen_category - 1]
+    category = @category_list[chosen_category - 1]
       #calls the get_organizations method from the Category Class to scrape organization data from the website
       #the method below should shovel all the scraped organizations into @organizations array
-      category.get_organizations
+    category.get_organizations
       puts "Here are all the charity organizations under the #{category.title}"
       #binding.pry
-      category.organizations.each.w=[ith_index(1) do |organization, index|
-        puts "#{index}. #{organization.category.title} - #{organization.title}" #if index == chosen_category
+    category.organizations.each.w=[ith_index(1) do |organization, index|
+      puts "#{index}. #{organization.category.title} - #{organization.title}" #if index == chosen_category
       #  binding.pry
-      end
-      #need to insert the scraper class here to provide the charity organization names and the specific metrics for them"
-      #CharityList::Charitylist.all.each_with_index do | charity|
-        #  puts charity.name
-      #end.
   end
+
 
   def get_user_organization(category)
     puts "Choose an organization to see its metrics"
     input = gets.strip
     organization = category.organizations[input.to_i - 1]
     organization.get_user_organization
-  end
-
-  def get_organization_metrics
   end
 end
--- lib/charity_list/scraper.rb
+++ lib/charity_list/scraper.rb
@@ -2,12 +2,13 @@ class CharityList::Scraper
     def self.scrape_categories
         doc = Nokogiri::HTML(open("https://www.charitywatch.org/top-rated-charities/all-charities"))
 
-        categories = doc.css("div#list-all.tab-pane.fade.show.active h2.mb-3")
-
-        category_names = categories.each do |c|
-            title = c.text.strip
-            CharityList::Category.new(title)
-        end
+        #categories = doc.css("div#list-all.tab-pane.fade.show.active h2.mb-3")
+        categories = doc.css("div#list-all.tab-pane.fade.show.active")
+        puts categories
+        #category_names = categories.each do |c|
+        #    title = c.text.strip
+        #    CharityList::Category.new(title)
+        #end
     end
 
     def self.scrape_organizations(category)
