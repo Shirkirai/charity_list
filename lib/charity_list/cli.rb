44bb53b16c693c684e9920c972c225a97ee18d9c
Jung Lee <lee.jung.s95@gmail.com>
2020-12-20 07:11:26 +0000

    .

--- lib/charity_list/cli.rb
+++ lib/charity_list/cli.rb
@@ -21,7 +21,7 @@ class CharityList::CLI
   def list_charity_categories
       puts "\nPlease type in the number corresponding to a specific category to see charity organizations listed under it\n"
       @category_list.each.with_index(1) do |category, index|
-          puts "#{index}. #{category.title}" #had to put category.title to print out the actual names of the categories
+        puts "#{index}. #{category.title}" #had to put category.title to print out the actual names of the categories
       end
   end
 
@@ -30,7 +30,7 @@ class CharityList::CLI
       chosen_category = gets.strip.to_i
       show_charity_organizations_for(chosen_category) if valid_input(chosen_category, @category_list)
       if !valid_input(chosen_category, @category_list)
-        get_user_charity_category
+        get_user_charity_category until valid_input
       end
   end
 
@@ -42,22 +42,23 @@ class CharityList::CLI
   def show_charity_organizations_for(chosen_category)
       #category is the category within the @@all array corresponding to the user input.
       #for example, if the user input is 2, then it'll take the 2nd entry from the array. In this case, it'll be 2. AIDS Charities
-    category = @category_list[chosen_category - 1]
+      category = @category_list[chosen_category - 1]
       #calls the get_organizations method from the Category Class to scrape organization data from the website
       #the method below should shovel all the scraped organizations into @organizations array
-    category.get_organizations
+      category.get_organizations
       puts "Here are all the charity organizations under the #{category.title}"
       #binding.pry
-    category.organizations.each.w=[ith_index(1) do |organization, index|
-      puts "#{index}. #{organization.category.title} - #{organization.title}" #if index == chosen_category
+      category.organizations.each.with_index(1) do |organization, index|
+        puts "#{index}. #{organization.category.title} - #{organization.title}" #if index == chosen_category
       #  binding.pry
+      end
   end
 
 
   def get_user_organization(category)
-    puts "Choose an organization to see its metrics"
-    input = gets.strip
-    organization = category.organizations[input.to_i - 1]
-    organization.get_user_organization
+      puts "Choose an organization to see its metrics"
+      input = gets.strip
+      organization = category.organizations[input.to_i - 1]
+      organization.get_user_organization
   end
 end
--- lib/charity_list/scraper.rb
+++ lib/charity_list/scraper.rb
@@ -2,9 +2,9 @@ class CharityList::Scraper
     def self.scrape_categories
         doc = Nokogiri::HTML(open("https://www.charitywatch.org/top-rated-charities/all-charities"))
 
-        #categories = doc.css("div#list-all.tab-pane.fade.show.active h2.mb-3")
-        categories = doc.css("div#list-all.tab-pane.fade.show.active")
-        puts categories
+        categories = doc.css("div#list-all.tab-pane.fade.show.active h2.mb-3")
+        #categories = doc.css("div#list-all.tab-pane.fade.show.active")
+        #puts categories.text.strip
         #category_names = categories.each do |c|
         #    title = c.text.strip
         #    CharityList::Category.new(title)
